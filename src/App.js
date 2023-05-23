a096a83be97be73ee28ed6f3fbecb1578995dc2d
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-23 17:44:39 +0530

    creating Routers both methods

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoutesFromElements, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Items from "./pages/Items";
+
+// Methor 1
+// function App() {
+//   const router = createBrowserRouter([
+//     { path: "/", element: <Home /> },
+//     { path: "/about", element: <About /> },
+//     { path: "/items", element: <Items /> },
+//   ]);
+
+// Method 2
+function App() {
+  const routes = createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/items" element={<Items />} />
+    </>
+  );
+  const router = createBrowserRouter(routes);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
