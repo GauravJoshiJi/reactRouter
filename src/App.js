bce6587f52536e43e51634b4489e87a920f746bf
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-23 19:17:46 +0530

    NavBar(NestedRouter)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createRoutesFromElements, Route } from "react-router-dom";
+// import { createRoutesFromElements, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Items from "./pages/Items";
+import Navbar from "./components/Navbar";
 
 // Methor 1
-// function App() {
-//   const router = createBrowserRouter([
-//     { path: "/", element: <Home /> },
-//     { path: "/about", element: <About /> },
-//     { path: "/items", element: <Items /> },
-//   ]);
-
-// Method 2
 function App() {
-  const routes = createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/items" element={<Items />} />
-    </>
-  );
-  const router = createBrowserRouter(routes);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/items", element: <Items /> },
+      ],
+    },
+  ]);
+
+  // Method 2
+  // function App() {
+  //   const routes = createRoutesFromElements(
+  //     <>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="/about" element={<About />} />
+  //       <Route path="/items" element={<Items />} />
+  //     </>
+  //   );
+  //   const router = createBrowserRouter(routes);
 
   return (
     <>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
       <main>
         <h1>Home Page</h1>
-        <Link to="/about">About</Link>&nbsp,&nbsp;
-        <Link to="/items">Items</Link>
       </main>
     </>
   );
